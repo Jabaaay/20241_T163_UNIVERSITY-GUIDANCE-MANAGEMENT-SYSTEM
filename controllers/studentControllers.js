da414d233391d34b1e1d13184e8f028e592a37e2
Mark Tristan T Jordan <2201101776@student.buksu.edu.ph>
2024-10-28 18:55:23 +0800

    Layering Controllers

---
+++ controllers/studentControllers.js
@@ -0,0 +1,31 @@
+// Get All History
+const getHistory = async (req, res) => {
+    
+}
+
+// Get the status of the student
+const getStatus = async (req, res) => {
+
+}
+
+//student can delete appointment
+const deleteAppointment = async (req, res) => {
+
+}
+
+//Delete Student by thier ID
+const addAppointment = async (req, res) => {
+
+}
+
+//Student can update Appointment
+const updateAppointment = async (req, res) => {
+
+}
+
+// Student can send a message if there any concerns
+const sendMessage = async (req, res) => {
+
+}
+
+export {getHistory, getStatus, deleteAppointment, addAppointment, updateAppointment, sendMessage};
