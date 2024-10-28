a5e42a14fc1a8048a372a6ef83ce9bb5c4d934b1
Mark Tristan T Jordan <2201101776@student.buksu.edu.ph>
2024-10-28 18:58:01 +0800

    Layering Controllers

---
+++ controllers/adminControllers.js
@@ -0,0 +1,32 @@
+// Get All History
+const getAppointment = async (req, res) => {
+    
+}
+
+// Admin can confirm the appointment by the student
+const confirmAppointment = async (req, res) => {
+
+}
+
+//Admin can view all history appointments
+const appointmentHistory = async (req, res) => {
+
+}
+
+// Admin can add Staff as Manage the system
+const addStaff = async (req, res) => {
+
+}
+// Admin can post announcements
+const postAnnouncements = async (req, res) => {
+
+}
+
+// Admin can get messages for those who have concerns
+const getMessage = async (req, res) => {
+
+}
+
+
+
+export {getAppointment, confirmAppointment, appointmentHistory, addStaff, postAnnouncements, getMessage};
---
+++ controllers/staffControllers.js
@@ -0,0 +1,28 @@
+// Get All History
+const getAppointment = async (req, res) => {
+    
+}
+
+// Staff can confirm the appointment by the student
+const confirmAppointment = async (req, res) => {
+
+}
+
+//Staff can view all history appointments
+const appointmentHistory = async (req, res) => {
+
+}
+
+// Staff can post announcements
+const postAnnouncements = async (req, res) => {
+
+}
+
+// Staff can get messages for those who have concerns
+const getMessage = async (req, res) => {
+
+}
+
+
+
+export {getAppointment, confirmAppointment, appointmentHistory, postAnnouncements, getMessage};
