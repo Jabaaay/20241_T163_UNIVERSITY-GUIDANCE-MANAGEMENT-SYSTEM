3094869b918e0518d4dbbe2a4648c484762dfe67
Jabaaay <2201101546@student.buksu.edu.ph>
2024-10-28 12:58:17 +0800

    Clean Code

--- app.js
+++ app.js
@@ -2,64 +2,64 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World');
 })
 
 
 app.get('/login', function (req, res) {
-    res.send('Log In as User')
+    res.send('Log In as User');
   })
 
   app.get('/landing_page', function (req, res) {
-    res.send('Buksu Guidance')
+    res.send('Buksu Guidance');
   })
 
   app.get('/announcement', function (req, res) {
-    res.send('Announcement')
+    res.send('Announcement');
   })
 
   app.get('/contact', function (req, res) {
-    res.send('Contact Us')
+    res.send('Contact Us');
   })
 
   app.post('/contact/message', function (req, res) {
-    res.send('Send Message')
+    res.send('Send Message');
   })
 
   app.get('/user_homepage', function (req, res) {
-    res.send('Scheduling')
+    res.send('Scheduling');
   })
 
   app.post('/user_homepage/confirm', function (req, res) {
-    res.send('Scheduling Confirm')
+    res.send('Scheduling Confirm');
   })
 
   app.get('/user_history', function (req, res) {
-    res.send('History Records of User')
+    res.send('History Records of User');
   })
 
   app.get('/user_status', function (req, res) {
-    res.send('Waiting for approval')
+    res.send('Waiting for approval');
   })
 
   app.put('/user_status/edit', function (req, res) {
-    res.send('Update Appointment')
+    res.send('Update Appointment');
   })
 
   app.delete('/user_status/delete', function (req, res) {
-    res.send('Delete Appointment')
+    res.send('Delete Appointment');
   })
 
   app.get('/user_profile', function (req, res) {
-    res.send('User Profile')
+    res.send('User Profile');
   })
 
   app.patch('/user_profile/edit', function (req, res) {
-    res.send('Update User Profile')
+    res.send('Update User Profile');
   })
 
   app.post('/user_status/edit', function (req, res) {
-    res.send('Update Appointment')
+    res.send('Update Appointment');
   })
 
 
--- app1.js
+++ app1.js
@@ -2,60 +2,60 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World');
 })
 
 
 app.get('/login/admin', function (req, res) {
-    res.send('Log In as Admin')
+    res.send('Log In as Admin');
   })
 
   app.get('/admin_dashboard', function (req, res) {
-    res.send('Dashboard')
+    res.send('Dashboard');
   })
 
   app.get('/admin_appoinment', function (req, res) {
-    res.send('Pending Appointments')
+    res.send('Pending Appointments');
   })
 
   app.get('/admin_reports', function (req, res) {
-    res.send('Report Generation')
+    res.send('Report Generation');
   })
 
   app.get('/post_announcements', function (req, res) {
-    res.send('Upload Announcements')
+    res.send('Upload Announcements');
   })
 
   app.post('/post_announcements/post', function (req, res) {
-    res.send('Post Annoucements')
+    res.send('Post Annoucements');
   })
 
   app.get('/admin_profile', function (req, res) {
-    res.send('Admin Profile')
+    res.send('Admin Profile');
   })
 
   app.patch('/admin_profile/edit', function (req, res) {
-    res.send('Update Admin Profile')
+    res.send('Update Admin Profile');
   })
 
   app.get('/notifications', function (req, res) {
-    res.send('Notifications')
+    res.send('Notifications');
   })
 
   app.get('/add_staff', function (req, res) {
-    res.send('Add Staff')
+    res.send('Add Staff');
   })
 
   app.post('/add_staff/send', function (req, res) {
-    res.send('Send Invites as Staff')
+    res.send('Send Invites as Staff');
   })
 
   app.get('/notifications/message', function (req, res) {
-    res.send('Messages')
+    res.send('Messages');
   })
 
   app.post('/notifications/message/reply', function (req, res) {
-    res.send('Send Messages')
+    res.send('Send Messages');
   })
 
 
